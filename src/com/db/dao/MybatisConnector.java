22c3b1aaa2e67984e2bd3f6458161a08d08cf52e
EunKwang <eks020@naver.com>
2015-04-06 23:13:04 +0900

    default settings

---
+++ src/com/db/dao/MybatisConnector.java
@@ -0,0 +1,32 @@
+package com.db.dao;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+public class MybatisConnector {
+	
+		private static SqlSessionFactory sqlSessionFactory;
+		static	{	
+			try{
+				String resource = "com/db/settings/MybatisConfig.xml";
+				InputStream inputStream = Resources.getResourceAsStream(resource);
+				
+				if(sqlSessionFactory ==null){
+					sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+				}
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}		
+		}
+		
+		public static SqlSessionFactory getSqlSessionFactory(){
+			return sqlSessionFactory;
+		}
+
+	}
+
