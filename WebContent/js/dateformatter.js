d7fd9c3b54ee3ce2643b0f067243004da0dec99f
ek <eks020@naver.com>
2015-05-25 15:33:01 +0900

    merging

--- WebContent/js/dateformatter.js
+++ WebContent/js/dateformatter.js
@@ -100,13 +100,8 @@ dateFormat.masks = {
 // Internationalization strings
 dateFormat.i18n = {
 	dayNames: [
-<<<<<<< HEAD
 		"일", "월", "화", "수", "목", "금", "토",
 		"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"
-=======
-		"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
-		"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
->>>>>>> bcd932d7b231f155ef36c372234bf32dabbb4c58
 	],
 	monthNames: [
 		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
--- src/com/servlet/test/testAction.java
+++ src/com/servlet/test/testAction.java
@@ -7,11 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.controller.RequestController;
-<<<<<<< HEAD
 import com.db.bean.TestBean;
-=======
-import com.db.bean.UserBean;
->>>>>>> bcd932d7b231f155ef36c372234bf32dabbb4c58
 import com.db.dao.testDao;
 
 @WebServlet(urlPatterns = "/testPage")
@@ -21,19 +17,11 @@ public class testAction extends RequestController{
 		// TODO Auto-generated method stub
 		System.out.println("in here");
 		
-<<<<<<< HEAD
 		ArrayList<TestBean> testList = testDao.getInstance().getUserList();
 		//String pass = ;
 		req.setCharacterEncoding("utf-8");
 		req.setAttribute("list", testList);
 		//req.setAttribute();
-=======
-		ArrayList<UserBean> testList = testDao.getInstance().getUserList();
-		
-		req.setCharacterEncoding("utf-8");
-		req.setAttribute("list", testList);
-		
->>>>>>> bcd932d7b231f155ef36c372234bf32dabbb4c58
 		return "view/test.jsp";
 	}
 }
